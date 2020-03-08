da958cddfb172cdb6ec86281fb85c9115a9ffc68
Zach Beever <zach.beever@gmail.com>
2020-03-08 16:21:18 -0400

    Cleaned up some of the code. Added the dipole model of Earth's magnetic field.

--- solver.py
+++ solver.py
@@ -1,8 +1,9 @@
-import numpy as np
-from scipy import signal
 import sys
 import math as mt
-from scipy.integrate import odeint
+
+import numpy as np
+from scipy import signal
+
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -10,13 +11,15 @@ axis_num = {'x' : 0,
             'y' : 1,
             'z' : 2}
 
+relativistic = True
+
 mu0 = 1.256e-6 # H/m
 epsilon0 = 8.854e-12 # F/m
 me = 9.109e-31 # kg
 mp = 1.672e-27 # kg
 qe = 1.602e-19 # C
 c = 3e8 # m/s
-relativistic = True
+Re = 6.371e6 # m
 
 class System:
     def __init__(self, electric_field, magnetic_field):
@@ -28,12 +31,11 @@ class System:
         self.particles.append(particle)
 
     def plot(self, T, dt):
+        steps = int(mt.ceil(T / dt))
         fig = plt.figure(figsize=plt.figaspect(0.5)*1.5)
         ax = fig.gca(projection='3d')
-        # fig, axs = plt.subplots(3)
 
         for particle in self.particles:
-            steps = int(mt.ceil(T / dt))
 
             R = np.zeros((steps, 3))
             V = np.zeros((steps, 3))
@@ -43,38 +45,38 @@ class System:
 
             for t in range(steps):
                 r, v = particle.advance(self.e_field, self.b_field, dt)
+
                 mu[t] = particle.moment(self.b_field)
                 B[t] = np.linalg.norm(self.b_field.at(r))
                 wn[t] = (abs(particle.q) * B[t]) / particle.m
+
                 R[t, :] = r
                 V[t, :] = v
 
-            # axs[0].plot(wn)
-            # axs[1].plot(R[:, 0], label='x')
-            # axs[1].plot(R[:, 1], label='y')
-            # axs[1].plot(R[:, 2], label='z')
-            # axs[1].legend()
-            # axs[2].plot(B)
-            # plt.plot(X_gca, Y_gca, Z_gca, '--')
-            max_range = np.array([R[:,0].max()-R[:,0].min(), R[:,1].max()-R[:,1].min(), R[:,2].max()-R[:,2].min()]).max() / 2.
-            mid_x = (R[:,0].max()+R[:,0].min()) * 0.5
-            mid_y = (R[:,1].max()+R[:,1].min()) * 0.5
-            mid_z = (R[:,2].max()+R[:,2].min()) * 0.5
-            ax.set_xlim(mid_x - max_range, mid_x + max_range)
-            ax.set_ylim(mid_y - max_range, mid_y + max_range)
-            ax.set_zlim(mid_z - max_range, mid_z + max_range)
             plt.plot(R[:, 0], R[:, 1], R[:, 2])
-        # ax.set_xlabel('X')
-        # ax.set_ylabel('Y')
-        # ax.set_zlabel('Z')
+
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+
+        # Temporary solution to scale the axes before plotting
+        ax.auto_scale_xyz([2*Re, 6*Re], [-2*Re, 2*Re], [-2*Re, 2*Re])
         plt.show()
 
 class Particle:
-    def __init__(self, r0, v0, q, m):
+    def __init__(self, r0, v_dir, E, q, m):
+        v = 0
+
+        if relativistic is False:
+            v = np.sqrt(2 * E * 1.602e-19 / m)
+        else:
+            gamma = (E * 1.602e-19) / (m * c**2) + 1
+            v = c * np.sqrt(1 - gamma**(-2)) # m/s
+
         self.q = q
         self.m = m
         self.r = r0
-        self.v = v0
+        self.v = v_dir / np.linalg.norm(v_dir) * v
 
     def v_par(self, b_field):
         B = b_field.at(self.r)
@@ -159,17 +161,33 @@ class DipoleField(Field):
         self.p[axis_num[axis]] = d * q
 
     def at(self, r):
+        r_mag = np.linalg.norm(r)
+        r_unit = r / r_mag
+        k = 1 / (4 * np.pi * epsilon0)
+
+        return k * (3 * np.dot(self.p, r_unit) * r_unit - self.p) * r_mag**(-3)
+
+class EarthDipole(Field):
+    # The dipole model of the Earth's magnetic field.
+
+    def __init__(self):
+        self.M = -8e15
+
+    def at(self, r):
         [x, y, z] = r
         R = np.sqrt(x**2 + y**2 + z**2)
-        M = -8e15
 
-        B_x = 3*M * (x*z) / (R**5)
-        B_y = 3*M * (y*z) / (R**5)
-        B_z = M * (3*z**2 - R**2) / (R**5)
+        B_x = 3 * self.M * (x * z) / (R**5)
+        B_y = 3 * self.M * (y * z) / (R**5)
+        B_z = self.M * (3 * z**2 - R**2) / (R**5)
 
         return np.array([B_x, B_y, B_z])
 
+
 def plot_field(field, x_len, y_len, z_len, nodes):
+    # Given one of the above fields, the dimensions of the area to plot, and the nodes
+    # at which to display the field values, plots the field.
+
     x = np.linspace(-x_len * 0.5, x_len * 0.5, nodes)
     y = np.linspace(-y_len * 0.5, y_len * 0.5, nodes)
     z = np.linspace(-z_len * 0.5, z_len * 0.5, nodes)
@@ -190,28 +208,17 @@ def plot_field(field, x_len, y_len, z_len, nodes):
 if __name__ == '__main__':
     # Setup the electric and magnetic fields of the system. Superposition to be implemented in the future.
     e_field = UniformField(0, 'z')
-    B = 1e8 # -8e15
-    b_field = DipoleField(B, 1, 'z')
+    b_field = EarthDipole()
 
     # Create the system to be studied.
     system = System(e_field, b_field)
 
-    # x = np.linspace(-5, 5, 20)
-    # y = np.linspace(-5, 5, 20)
-
-    # xv, yv = np.meshgrid(x, y)
-
-    E = 50e6 # eV
-    gamma = E / (0.511e6) + 1
-    v = c * np.sqrt(1 - gamma**(-2)) # m/s
     # Add particles. The first array is the particle's position, the second is the particle's velocity.
-    # The third argument is the particle's charge and the fourth is its mass.
-    d = 5 * 6.371e6
-    theta = np.pi / 4
-    system.addParticle( Particle(np.array([d, 0., 0.]), np.array([0., v * np.cos(theta), v * np.sin(theta)]), -qe, me) )
+    # The third argument is the particle's energy (in eV), the fourth is its charge and the fifth is its mass.
+
+    system.addParticle( Particle(np.array([5 * Re, 0., 0.]), np.array([0., 0.5, 1.]), 50e6, -qe, me) )
 
     # Call these last. They solve the trajectories and plot the solutions
     T = 1
     dt = 1e-4
     system.plot(T, dt)
-    # plot_field(b_field, 10, 10, 10, 8)
