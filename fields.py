6a8bc958acf6b0f426635f8347cb813f8af85b9e
Zach Beever <zach.beever@gmail.com>
2020-06-08 11:05:13 -0400

    Added the 4 Tsyganenko models and documented their input parameters.

--- fields.py
+++ fields.py
@@ -70,8 +70,50 @@ class EarthDipole(Field):
         return np.array([B_x, B_y, B_z])
 
 class Tsyganenko89(Field):
+    """A model of Earth's magnetic field consisting of a superposition of the Tsyganenko 1989 model (DOI: 10.1016/0032-0633(89)90066-4) and the IGRF model.
+
+    Args:
+    Kp (int): A mapping to the Kp geomagnetic activity index. Acceptable values range from 1 to 7, mapping to values between 0 and 10, inclusive.
+    """
+
     def __init__(self, Kp_):
         self.Kp = Kp_
+        
+    def at(self, r, t = 4.01e7, sw_v = np.array([-400., 0., 0.])):
+        """Get the magnetic field vector at the given location.
+
+        Args:
+        r (numpy array): The desired position (in GSM coordinates).
+        t (int): Universal time (in seconds). Defaults to a value that gives a magnetotail aligned with the x axis.
+        sw_v (numpy array): The solar wind velocity vector (in GSM coordinates). Defaults to [-400, 0, 0].
+        """
+        k = 1. / Re
+        x_gsm = r[0] * k
+        y_gsm = r[1] * k
+        z_gsm = r[2] * k
+        
+        ps = gp.recalc(t, sw_v[0], sw_v[1], sw_v[2])
+        bx, by, bz = gp.igrf_gsm(x_gsm, y_gsm, z_gsm)
+        dbx, dby, dbz = gp.t89.t89(self.Kp, ps, x_gsm, y_gsm, z_gsm)
+        
+        return np.array([bx + dbx, by + dby, bz + dbz]) * 1e-9
+
+class Tsyganenko96(Field):
+    """
+    A model of Earth's magnetic field consisting of a superposition of the Tsyganenko 1996 model (DOI: 10.1029/96JA02735) and the IGRF model.
+
+    Args:
+    par (array): A 10-element array containing the model parameters.
+
+    par[0] (Pdyn): The solar wind dynamic pressure in nPa. Typically in the range of 1 to 6 nPa.
+    par[1] (Dst): The disturbance storm-time index, a measure of magnetic activity connected to the ring current. Values are measured in nT. A value less than -50 nT indicates high geomagnetic activity. 
+    par[2] (ByIMF): The y component of the interplanetary magnetic field in nT. Total strength usually ranges from 1 to 37 nT.
+    par[3] (BzIMF): The z component of the interplanetary magnetic field in nT. Total strength usually ranges from 1 to 37 nT.
+    par[4-9]: Not used
+    """
+
+    def __init__(self, par_):
+        self.par = par_
         
     def at(self, r, t = 4.01e7, sw_v = np.array([-400., 0., 0.])):
         k = 1. / Re
@@ -81,7 +123,72 @@ class Tsyganenko89(Field):
         
         ps = gp.recalc(t, sw_v[0], sw_v[1], sw_v[2])
         bx, by, bz = gp.igrf_gsm(x_gsm, y_gsm, z_gsm)
-        dbx, dby, dbz = gp.t89.t89(self.Kp, ps, x_gsm, y_gsm, z_gsm)
+        dbx, dby, dbz = gp.t96.t96(self.par, ps, x_gsm, y_gsm, z_gsm)
+        
+        return np.array([bx + dbx, by + dby, bz + dbz]) * 1e-9
+
+class Tsyganenko01(Field):
+    """
+    A model of Earth's magnetic field consisting of a superposition of the Tsyganenko 2001 model (DOI: 10.1029/2001JA000220) and the IGRF model.
+
+    Args:
+    par (array): A 10-element array containing the model parameters.
+
+    par[0] (Pdyn): The solar wind dynamic pressure in nPa. Typically in the range of 1 to 6 nPa.
+    par[1] (Dst): The disturbance storm-time index, a measure of magnetic activity connected to the ring current. Values are measured in nT. A value less than -50 nT indicates high geomagnetic activity. 
+    par[2] (ByIMF): The y component of the interplanetary magnetic field in nT. Total strength usually ranges from 1 to 37 nT.
+    par[3] (BzIMF): The z component of the interplanetary magnetic field in nT. Total strength usually ranges from 1 to 37 nT.
+    par[4] (G1): A parameter capturing the cross-tail current's dependence on the solar wind. Mathematically, it is defined as the average of V*h(B_perp)*sin^3(theta/2) where V is the solar wind speed, B_perp is the transverse IMF component, theta is the IMF's clock angle, and h is a function that behaves as B_perp^2 for common values of the IMF. A typical value is 6.
+    par[5] (G2): A parameter capturing the earthward / tailward shift of the tail current, defined as the average of a*V*Bs, where V is the solar wind speed, Bs is the southward component of the IMF (|Bz| for Bz < 0, 0 otherwise) and a = 0.005. A typical value is 10.
+    par[6-9]: Not used
+    """
+
+    def __init__(self, par_):
+        self.par = par_
+        
+    def at(self, r, t = 4.01e7, sw_v = np.array([-400., 0., 0.])):
+        k = 1. / Re
+        x_gsm = r[0] * k
+        y_gsm = r[1] * k
+        z_gsm = r[2] * k
+        
+        ps = gp.recalc(t, sw_v[0], sw_v[1], sw_v[2])
+        bx, by, bz = gp.igrf_gsm(x_gsm, y_gsm, z_gsm)
+        dbx, dby, dbz = gp.t01.t01(self.par, ps, x_gsm, y_gsm, z_gsm)
+        
+        return np.array([bx + dbx, by + dby, bz + dbz]) * 1e-9
+
+class Tsyganenko04(Field):
+    """
+    A model of Earth's magnetic field consisting of a superposition of the Tsyganenko 2004 model (DOI: 10.1029/2004JA010798) and the IGRF model.
+
+    Args:
+    par (array): A 10-element array containing the model parameters.
+
+    par[0] (Pdyn): The solar wind dynamic pressure in nPa. Typically in the range of 1 to 6 nPa.
+    par[1] (Dst): The disturbance storm-time index, a measure of magnetic activity connected to the ring current. Values are measured in nT. A value less than -50 nT indicates high geomagnetic activity. 
+    par[2] (ByIMF): The y component of the interplanetary magnetic field in nT. Total strength usually ranges from 1 to 37 nT.
+    par[3] (BzIMF): The z component of the interplanetary magnetic field in nT. Total strength usually ranges from 1 to 37 nT.
+    par[4] (Wt1): Driving parameter of the inner part of the tail field. Saturates at 0.71 +- 0.05. Peak estimate: 4 to 12.
+    par[5] (Wt2): Driving parameter of the outer part of the tail field. Saturates at 0.39 +- 0.05. Peak estimate: 3 to 7.
+    par[6] (Ws): Driving parameter of the axially symmetric part of the ring current. Saturates at 3.3 +- 0.5. Peak estimate: 4 to 15.
+    par[7] (Wp): Driving parameter of the partial ring current field. Saturates at 75 +- 30. Peak estimate 10 to 50.
+    par[8] (Wb1): Driving parameter of the principle mode of the Birkeland current. Saturates at 6.4 +- 1.0. Peak estimate 7 to 30.
+    par[9] (Wb2): Driving parameter of the secondary mode of the Birkeland current. Saturates at 0.88 +- 0.06. Peak estimate 20 to 100.
+
+    """
+    def __init__(self, par_):
+        self.par = par_
+        
+    def at(self, r, t = 4.01e7, sw_v = np.array([-400., 0., 0.])):
+        k = 1. / Re
+        x_gsm = r[0] * k
+        y_gsm = r[1] * k
+        z_gsm = r[2] * k
+        
+        ps = gp.recalc(t, sw_v[0], sw_v[1], sw_v[2])
+        bx, by, bz = gp.igrf_gsm(x_gsm, y_gsm, z_gsm)
+        dbx, dby, dbz = gp.t04.t04(self.par, ps, x_gsm, y_gsm, z_gsm)
         
         return np.array([bx + dbx, by + dby, bz + dbz]) * 1e-9
 
