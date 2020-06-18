308e23738065b9b55dee4cd1de286f07cdb5b879
Zach Beever <zach.beever@gmail.com>
2020-06-18 15:46:57 -0400

    Removed obsolete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize("analytical_fields.pyx", compiler_directives = {'language_level' : "3"}, annotate=True),
-    zip_safe = False
-)
